0170c5d4c0f5a942bb0c7ed39279b985287a37ef
Anu and Sri Kemburu <avisfy0@hotmail.com>
2022-03-01 21:14:58 -0500

    submit homework:

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # callbackHomework
+
+## Mode2/Week1/Day1 Homework
+
+❓ When the add and subtract functions are passed as arguments to the compute function - they are ___ functions.
+
+**Answer: Callback**
+
+❓How many times would the anonymous callback function below be called?**:
+
+const flowers = ['rose', 'orchid', 'daisy'];
+flowers.forEach(function(flower, idx) {
+  console.log(`${idx + 1}) ${flower}`);
+});
+
+**Answer: 3 times**
+
+❓ In your own words describe what the term "implicit return" means.
+
+**Answer: A function that returnes values without using the return keyword, it’s called an implicit return.**
+
+❓ Describe what a callback function is in your own words.
+
+**Answer: A callback function is a function passed into another function as an argument, which is then invoked inside the outer function to complete some kind of routine or action.**
--- callback.js
+++ callback.js
@@ -141,3 +141,21 @@ console.log("add12 = " + add12)
 
 // Syntax error, must be an expression
 //const add13 = (x, y) => return x + y;
+
+console.log("\n===To implicitly return a JS object, wrap it in parentheses to avoid the curly braces of the object being interpreted as a statement block===\n")
+
+let todos = ['Buy milk', 'Mow lawn'];
+
+// Below line of code won't work - looks like a statement block
+// let todoObjects = todos.map(todo => {todo: todo, done: false});
+
+// Wrap the implicit returned object in parens
+let todoObjects = todos.map(todo => ({todo: todo, done: false}));
+
+console.log("\n===All arrow functions are expressions. There's no such thing as an arrow function definition/declaration===\n")
+
+// Nope, syntax error (no declarations for arrow functions)
+//add(x, y) => x + y;
+
+// This is what you want - a function expression
+const add21 = (x, y) => x + y;
